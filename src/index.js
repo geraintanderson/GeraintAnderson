dbcb31fb5281faeba4279081cb84830ab76463ce
Geraint Anderson <geraint@geraintanderson.com>
2018-06-24 12:32:35 +0100

    Added code to style the active tab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// window.alert('test')
+// (function () {
+//   // Set the active class on the navbar
+//   console.log('--- test')
+//
+//   console.log(window.location.pathname)
+//
+//   document.onload()
+// })()
+window.addEventListener('load', function () {
+  console.log(window.location.pathname)
+  parts = window.location.pathname.split('/')
+  var page = parts[parts.length - 1]
+  // Remove the file extension
+  var elemId = 'nav-' + page.split('.')[0]
+  console.log(elemId)
+
+  document.getElementById(elemId).classList.add('selected')
+})
