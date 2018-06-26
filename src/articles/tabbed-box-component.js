cb471a0468521481a917fe0ef986b23f5922b7dc
Geraint Anderson <geraint@geraintanderson.com>
2018-06-26 23:11:39 +0100

    Added an article about the Coop responsive tab component

--- src/articles/tabbed-box-component.js
+++ src/articles/tabbed-box-component.js
@@ -1,23 +1,79 @@
-// There is no JS required for demo 1
-
-// Demo 2
 (function () {
   window.onload = function () {
-
-    const tabComponent = document.getElementById('tab-component-two')
-    const tabItems = Array.from(tabComponent.children)
-
-    const clearSelectedClasses = function () {
-      tabItems.forEach(elem => {
-        elem.className = elem.className.replace(/\bselected\b/g, '')
-      })
-    }
-
-    tabItems
-    .forEach(elem => elem.onclick = function () {
-      clearSelectedClasses()
-      elem.classList.add('selected')
-    })
-
+    console.log('---- loaded')
+    demo2()
+    demo3()
+    demo4()
+    demo5()
   }
-})()
+})();
+
+// There is no JS required for demo 1
+
+const demo2 = function () {
+  const tabComponent = document.getElementById('tab-component-two')
+  const tabItems = Array.from(tabComponent.children)
+
+  const clearSelectedClasses = function () {
+    tabItems.forEach(elem => {
+      elem.className = elem.className.replace(/\bselected\b/g, '')
+    })
+  }
+
+  tabItems
+  .forEach(elem => elem.onclick = function () {
+    clearSelectedClasses()
+    elem.classList.add('selected')
+  })
+}
+
+const demo3 = function () {
+  const tabComponent = document.getElementById('tab-component-three')
+  const tabItems = Array.from(tabComponent.children)
+
+  const clearSelectedClasses = function () {
+    tabItems.forEach(elem => {
+      elem.className = elem.className.replace(/\bselected\b/g, '')
+    })
+  }
+
+  tabItems
+  .forEach(elem => elem.onclick = function () {
+    clearSelectedClasses()
+    elem.classList.add('selected')
+  })
+}
+
+const demo4 = function () {
+  const tabComponent = document.getElementById('tab-component-four')
+  const tabItems = Array.from(tabComponent.children)
+
+  const clearSelectedClasses = function () {
+    tabItems.forEach(elem => {
+      elem.className = elem.className.replace(/\bselected\b/g, '')
+    })
+  }
+
+  tabItems
+  .forEach(elem => elem.onclick = function () {
+    clearSelectedClasses()
+    elem.classList.add('selected')
+  })
+}
+
+const demo5 = function () {
+  const tabComponent = document.getElementById('tab-component-five')
+  const tabItems = Array.from(tabComponent.children)
+
+  const clearSelectedClasses = function () {
+    tabItems.forEach(elem => {
+      elem.className = elem.className.replace(/\bselected\b/g, '')
+    })
+  }
+
+  tabItems
+  .forEach(elem => elem.onclick = function () {
+    clearSelectedClasses()
+    elem.classList.add('selected')
+  })
+}
