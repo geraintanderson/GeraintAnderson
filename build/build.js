d1bfd61224de6da05ba63e51cf0f309a35d58c73
Geraint Anderson <geraint@geraintanderson.com>
2018-07-16 22:59:08 +0100

    Added a node.js builder to compile the site pages

--- build/build.js
+++ build/build.js
@@ -2,8 +2,63 @@
 //  node build.js
 const fs = require('fs').promises
 const path = require('path')
+const handlebars = require('handlebars')
+const _ = require('underscore')
 const buildDir = 'public_html'
 const srcDir = 'src'
+
+function buildPage (config) {
+  // Config: {
+  //  destDir: string,
+  //  styles: [string],
+  //  scripts: [string],
+  //  html: string,
+  //  outFile: string
+  // }
+
+  function getNestingPrefix (homeDir, thisDir) {
+    const relativePath = path.relative(homeDir, thisDir)
+    const nestingLayers = relativePath.split(path.sep).filter(path => path !== '').map(() => '..').join(path.sep)
+    const nestingPrefix = nestingLayers ? nestingLayers + '/' : ''
+    console.log('---- nesting prefix for ' + thisDir + ' = ' + nestingPrefix)
+    console.log(homeDir + ' - ' + thisDir)
+    console.log(relativePath)
+    console.log(relativePath.split(path.sep))
+    return nestingPrefix
+  }
+
+  return new Promise ((resolve, reject) => {
+    console.log(config)
+    Promise.all(
+      config.styles.map(file => {
+        fs.copyFile(file, path.join(config.destDir, path.basename(file)))
+      })
+    )
+    .then(() => Promise.all(
+      config.scripts.map(file => {
+        fs.copyFile(file, path.join(config.destDir, path.basename(file)))
+      })
+    ))
+    .then(() => Promise.all([
+      fs.readFile(path.join(srcDir, 'page-frame.html'), 'utf-8'),
+      fs.readFile(config.html, 'utf-8')
+    ]))
+    .then(sources => {
+      const template = handlebars.compile(sources[0])
+      const context = _.clone(config)
+      context.styles = context.styles.map(style => path.basename(style))
+      context.scripts = context.scripts.map(script => path.basename(script))
+      // XXX Do the same for scripts
+      context.nestingPrefix = getNestingPrefix(buildDir, config.destDir)
+      context.pageContent = sources[1]
+      return template(context)
+    })
+    .then(rendered => fs.writeFile(path.join(config.destDir, config.outFile), rendered))
+
+    .then(() => resolve(true))
+    .catch(err => reject(err))
+  })
+}
 
 fs.mkdir(buildDir)
 // Create empty folders
@@ -12,7 +67,7 @@ fs.mkdir(buildDir)
   fs.mkdir(path.join(buildDir, 'assets')),
   fs.mkdir(path.join(buildDir, 'articles'))
 ]))
-// Copy static files
+// Copy common static files
 .then(() => fs.readdir(path.join(srcDir, 'images')))
 .then((files) => {
   files.forEach(file => fs.copyFile(path.join(srcDir, 'images', file), path.join(buildDir, 'images', file)))
@@ -26,7 +81,51 @@ fs.mkdir(buildDir)
   fs.copyFile(path.join(srcDir, 'index.css'), path.join(buildDir, 'index.css'))
   fs.copyFile(path.join(srcDir, 'favicon.ico'), path.join(buildDir, 'favicon.ico'))
 })
-// Build the homepage - XXX Should use handlebars
+// Build the site pages
+.then(() => buildPage({
+  html: path.join(srcDir, 'index', 'homepage.html'),
+  outFile: 'index.html',
+  styles: [path.join(srcDir, 'index', 'homepage.css')],
+  scripts: [],
+  destDir: buildDir
+}))
+.then(() => buildPage({
+  html: path.join(srcDir, 'profile', 'profile.html'),
+  outFile: 'profile.html',
+  styles: [path.join(srcDir, 'profile', 'profile.css')],
+  scripts: [],
+  destDir: buildDir
+}))
+.then(() => buildPage({
+  html: path.join(srcDir, 'cv', 'cv.html'),
+  outFile: 'cv.html',
+  styles: [path.join(srcDir, 'cv', 'cv.css')],
+  scripts: [],
+  destDir: buildDir
+}))
+.then(() => buildPage({
+  html: path.join(srcDir, 'portfolio', 'portfolio.html'),
+  outFile: 'portfolio.html',
+  styles: [],
+  scripts: [],
+  destDir: buildDir
+}))
+.then(() => buildPage({
+  html: path.join(srcDir, 'articles', 'articles.html'),
+  outFile: 'articles.html',
+  styles: [path.join(srcDir, 'articles', 'articles.css')],
+  scripts: [],
+  destDir: buildDir
+}))
+.then(() => buildPage({
+  html: path.join(srcDir, 'articles', 'tabbed-box-component.html'),
+  outFile: 'tabbed-box-component.html',
+  styles: [path.join(srcDir, 'articles', 'tabbed-box-component.css')],
+  scripts: [path.join(srcDir, 'articles', 'tabbed-box-component.js')],
+  destDir: path.join(buildDir, 'articles')
+}))
+
 .catch(console.log)
 
 // XXX TODO NEXT: Finish this builder...
+// XXX tidy up the functions that are passed to the promise
