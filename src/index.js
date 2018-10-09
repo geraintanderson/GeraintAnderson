9336ccfe9cb528756776d25a6f7ce94b8bb94a6c
Geraint Anderson <geraint@geraintanderson.com>
2018-10-09 21:52:51 +0100

    Added social media links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// (function () {
-//   // Set the active class on the navbar
-//   console.log('--- test')
-//
-//   console.log(window.location.pathname)
-//
-//   document.onload()
-// })()
 window.addEventListener('load', function () {
+  // Style the navbar according to the current route
   parts = window.location.pathname.split('/')
   var page = parts[parts.length - 1]
   // Remove the file extension
@@ -15,5 +8,15 @@ window.addEventListener('load', function () {
   var elem = document.getElementById(elemId)
   if (elem) {
     elem.classList.add('selected')
+  }
+
+  // Add functionality to the social buttons
+  const twitterButtons = document.getElementsByClassName('twitter-button')
+  for (let i = 0; i < twitterButtons.length; i++) {
+    twitterButtons[i].href='https://twitter.com/share?url=' + escape(window.location.href) + '&text=%40Geraint_TJ_A'
+  }
+  const linkedinButtons = document.getElementsByClassName('linkedin-button')
+  for (let i = 0; i < linkedinButtons.length; i++) {
+    linkedinButtons[i].href='https://www.linkedin.com/shareArticle?url=' + escape(window.location.href) + '&text=%40Geraint_TJ_A'
   }
 })
